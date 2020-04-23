6193b42cb41d7056d4b9827fef5c72383ae49962
Xavier Carty <xaviercarty@gmail.com>
2020-04-22 21:17:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+const text = document.querySelector("#text")
+
+document.addEventListener("DOMContentLoaded", function() {
+     text.innerText = "This was really cool"
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("This was next")
+ });
+
+  console.log("This fire first")
+  
+  
+setTimeout(function(){
+ 	alert("Sup!"); 
+}, 2000);//wait 2 seconds
